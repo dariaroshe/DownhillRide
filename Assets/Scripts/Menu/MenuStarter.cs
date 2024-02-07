e1b97c38f51485462cde35796b244bc621799fd6
Daria <dariagrosheva99@gmail.com>
2024-02-07 21:51:00 +0300

    Add menu

---
+++ Assets/Scripts/Menu/MenuStarter.cs
@@ -0,0 +1,16 @@
+using System;
+using Game;
+using UnityEngine;
+
+namespace Menu
+{
+    public class MenuStarter : MonoBehaviour
+    {
+        public MenuScene MenuScene;
+
+        private void Start()
+        {
+            var menuModel = new MenuModel();
+        }
+    }
+}
